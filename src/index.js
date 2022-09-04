13394d6e82cbe7acd018adb5c12342bb9cfd5eb9
anjit pariyar <anjitpariyar@gmail.com>
2022-09-04 19:18:50 +0545

    update test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { StrictMode } from "react";
 // import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
+
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
